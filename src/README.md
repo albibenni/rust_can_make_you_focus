0828ca1167eb78e692185150ba319a752edd9db4
Alberto Benatti <albi.benni8@gmail.com>
2025-03-09 18:33:48 +0100

    yuppi, with eleveted privileges on cargo run write is working, base prj already done

---
+++ src/README.md
@@ -0,0 +1,4 @@
+## Why
+
+## How to use it
+- Run the cli app with elevated privileges, `sudo cargo run...` `sudo ./...`
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::read_to_string;
+
+const FILE_PATH: &str = "/etc/hosts";
+const LOCALHOST: &str = "127.0.0.1";
+fn main() -> Result<(), std::io::Error> {
+    println!("Hello, Let's focus!");
+    let mut file: String = read_to_string(FILE_PATH).expect("Failed to read file");
+
+    let web_site: &str = "www.example.com\n";
+    file.push_str(LOCALHOST);
+    file.push_str("               ");
+    file.push_str(web_site);
+    println!("{}", file);
+
+    let res = std::fs::write(FILE_PATH, file);
+    return res;
 }
